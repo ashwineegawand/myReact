57982c3321cfb55ece1a935e8e13480b9cd845a3
ashwineegawand <ashwineegawand@gmail.com>
2024-03-16 15:07:39 -0700

    added React Component and component composition

--- App.js
+++ App.js
@@ -9,6 +9,18 @@ const heading = (
     </h1>
 );
 
+const Title = () => (
+    <h1>Title Functional Compenent(composition inside Heading compenent)</h1>
+);
+
+const HeadingComponent = () => (
+    <div>
+        <Title />
+        <h1>React Functional Component</h1>
+    </div>
+    
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
