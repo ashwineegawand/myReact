32bc9b2b9cbe339b6e4355db0d067c6edc05e286
ashwineegawand <ashwineegawand@gmail.com>
2024-03-16 12:12:32 -0700

    react-with-jsx-react-element

--- App.js
+++ App.js
@@ -1,40 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div", 
-    {id:"parent"},
-    [
-        React.createElement(
-            "div", 
-            {id:"child1"},
-            [
-                React.createElement(
-                    "h1",
-                    {},
-                    "I'm an H1 tag"
-                ),
-                React.createElement(
-                    "h2",
-                    {},
-                    "I'm an H2 tag"
-                )
-            ]),
-            React.createElement(
-                "div", 
-                {id:"child2"},
-                [
-                    React.createElement(
-                        "h1",
-                        {},
-                        "I'm an H1 tag"
-                    ),
-                    React.createElement(
-                        "h2",
-                        {},
-                        "I'm an H2 tag"
-                    )
-                ])
-    ]);
+//React Element
+
+const heading = (
+    <h1 id="heading">
+       Hello from JSX React!
+    </h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(heading);
